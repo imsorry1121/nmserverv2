37124ae795a3d931d29af17de622fc8a33e9745a
cytms <cytmos@gmail.com>
2013-10-24 15:57:07 +0800

    modify the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,18 @@
 
 #blogger
 
-bimgpath = "app/assets/images/bloggers/"
-dimgpath = "app/assets/images/dishs/"
-simgpath = "app/assets/images/stores/"
-File.open(bimgpath+"1.jpeg", "rb") { |file| Blogger.create(:title=> "寧夏小霸王",:name=>"張宇婷", :img=>file.read, :intro=>"在地23年，嘗遍台灣美食，最愛寧夏，寧夏就是我的地盤。")  }
-File.open(bimgpath+"2.jpeg", "rb") { |file| Blogger.create(:title=>"台灣美食王", :name=>"陳世穎", :img=>file.read, :intro=>"寧夏是我第二個家。")  }
-File.open(bimgpath+"3.jpg", "rb") { |file| Blogger.create(:title=>"印度美食王", :name=>"楊筑雅", :img=>file.read, :intro=>"我最愛吃。")  }
+# bimgpath = "app/assets/images/bloggers/"
+# dimgpath = "app/assets/images/dishs/"
+# simgpath = "app/assets/images/stores/"
+# File.open(bimgpath+"1.jpeg", "rb") { |file| Blogger.create(:title=> "寧夏小霸王",:name=>"張宇婷", :img=>file.read, :intro=>"在地23年，嘗遍台灣美食，最愛寧夏，寧夏就是我的地盤。")  }
+# File.open(bimgpath+"2.jpeg", "rb") { |file| Blogger.create(:title=>"台灣美食王", :name=>"陳世穎", :img=>file.read, :intro=>"寧夏是我第二個家。")  }
+# File.open(bimgpath+"3.jpg", "rb") { |file| Blogger.create(:title=>"印度美食王", :name=>"楊筑雅", :img=>file.read, :intro=>"我最愛吃。")  }
+Blogger.delete_all
+File.open("public/bloggers/list.txt").each do |line|
+	puts line
+	arr = line.split("\t")
+	Blogger.create(:img => open("public/bloggers/#{arr[0]}").read, :name => arr[1], :title => arr[2], :intro => arr[3])
+end
 #comment
 
 #store
