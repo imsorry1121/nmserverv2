2f6af65ee82468e2931fe6b5966194fbae27fa78
cytms <cytmos@gmail.com>
2013-11-11 01:29:05 +0800

    add exception

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,28 +15,36 @@ Dish.delete_all
 File.open("public/dishes.txt").each do |line|
 	puts line
 	arr = line.split("\t")
-	Dish.create(
-		:id => arr[0],
-		:img => File.open("public/dishes/d#{arr[0]}.jpg", 'rb').read,
-		:name => arr[2],
-		:store_id => arr[1],
-		:price => arr[3]
-		)
+	begin
+		Dish.create(
+			:id => arr[0],
+			:img => File.open("public/dishes/d#{arr[0]}.jpg", 'rb').read,
+			:name => arr[2],
+			:store_id => arr[1],
+			:price => arr[3]
+			)
+	rescue
+		next
+	end
 end
 
 Store.delete_all
 File.open("public/stores.txt").each do |line|
 	puts line
 	arr = line.split("\t")
-	Store.create(
-		:img1 => File.open("public/stores/s#{arr[0]}.jpg", 'rb').read,
-		:name => arr[1],
-		:location => arr[2],
-		:otime => arr[3],
-		:ctime => arr[4],
-		:intro => arr[5],
-		:fb_url => arr[6]
-		)
+	begin
+		Store.create(
+			:img1 => File.open("public/stores/s#{arr[0]}.jpg", 'rb').read,
+			:name => arr[1],
+			:location => arr[2],
+			:otime => arr[3],
+			:ctime => arr[4],
+			:intro => arr[5],
+			:fb_url => arr[6]
+			)
+	rescue
+		next
+	end
 end
 
 #blogger
@@ -44,23 +52,31 @@ Blogger.delete_all
 File.open("public/bloggers/list.txt").each do |line|
 	puts line
 	arr = line.split("\t")
-	Blogger.create(
-		:img => File.open("public/bloggers/#{arr[0]}", 'rb').read, 
-		:name => arr[1], 
-		:title => arr[2], 
-		:intro => arr[3]
-		)
+	begin
+		Blogger.create(
+			:img => File.open("public/bloggers/#{arr[0]}", 'rb').read, 
+			:name => arr[1], 
+			:title => arr[2], 
+			:intro => arr[3]
+			)
+	rescue
+		next
+	end
 end
 
 Message.delete_all
 File.open("public/messages.txt").each do |line|
 	puts line
 	arr = line.split("\t")
-	Message.create(
-		:title => arr[1],
-		:img => File.open("public/messages/n#{arr[0]}.jpg", 'rb').read,
-		:article => arr[2],
-		:author => arr[3]
-		)
+	begin
+		Message.create(
+			:title => arr[1],
+			:img => File.open("public/messages/n#{arr[0]}.jpg", 'rb').read,
+			:article => arr[2],
+			:author => arr[3]
+			)
+	rescue
+		next
+	end
 end  
       
